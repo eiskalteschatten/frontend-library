caa2a6d8fb9b1078445937f7e044b4faeaf9dd6c
Alex Seifert <alex@alexseifert.com>
2022-07-25 11:55:51 +0200

    Remove the Next components from the eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,6 @@ module.exports = {
     'browser': true,
   },
   'extends': [
-    'next',
-    'next/core-web-vitals',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
   ],
