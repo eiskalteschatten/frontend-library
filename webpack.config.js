6769ab4ca633c3ae4c825b4d6248e1d12417c81c
Alex Seifert <alex@alexseifert.com>
2022-07-25 10:53:20 +0200

    Add the webpack config

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: '@alexseifert/',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[hash:base64:5]',
+              },
+            }
+          },
+          { loader: 'sass-loader' },
+          {
+            loader: 'sass-resources-loader',
+            options: {
+              resources: [
+                './src/styles/_breakpoints.scss',
+                './src/styles/_mixins.scss',
+                './src/styles/_variables.scss',
+                './src/styles/_functions.scss'
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack', 'url-loader'],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src/'),
+    },
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+};
