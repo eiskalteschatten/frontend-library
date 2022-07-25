e844a8358e1c33925c07ebc946263f331820cacc
Alex Seifert <alex@alexseifert.com>
2022-07-25 10:59:51 +0200

    Add the colors interface

---
+++ src/interfaces/colors.ts
@@ -0,0 +1,10 @@
+export enum PaletteColorClasses {
+  Blue = 'blue',
+  DarkBlue = 'darkBlue',
+  Gray = 'gray',
+  DarkGray = 'darkGray',
+  Orange = 'orange',
+  Yellow = 'yellow',
+  Red = 'red',
+  Green = 'green',
+}
