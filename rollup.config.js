00ae615644e9adb16802fcbc8a2ff7b346a92aec
Alex Seifert <alex@alexseifert.com>
2022-07-28 16:54:45 +0200

    Start configuring the SASS compilation

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import scss from 'rollup-plugin-scss';
 
 const packageJson = require('./package.json');
 
@@ -23,6 +24,7 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
+      scss(),
       typescript({ tsconfig: './tsconfig.json' }),
     ],
   },
