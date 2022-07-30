54552cb07b4f4d2412d24d5a3ac248e4dcaa9d01
Alex Seifert <alex@alexseifert.com>
2022-07-30 11:54:44 +0200

    Remove all build dirs before building

--- build-scripts/prebuild.js
+++ build-scripts/prebuild.js
@@ -1,3 +1,5 @@
 const fs = require('fs');
 
 fs.rmSync('./dist', { recursive: true, force: true });
+fs.rmSync('./static', { recursive: true, force: true });
+fs.rmSync('./styles', { recursive: true, force: true });
